0c6d190f1a8133b0d84e20a9d3822dfd652bb7bb
Daniel Isaac Geslin <danielisaacgeslin@gmail.com>
2018-02-19 11:35:53 +0100

    first

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  public render(): JSX.Element {
+    return (
+      <div className="text-red-darker">App</div>
+    );
+  }
+}
+
+export default App;
