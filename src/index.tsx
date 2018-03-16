e6fe628e40cb7affaa520604d7f014aeddb2d8a7
daniel <danielisaacgeslin@gmail.com>
2018-03-16 11:12:41 +0100

    removing tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import './/index.css';
+import './index.css';
 
 ReactDOM.render(
   <App />,
